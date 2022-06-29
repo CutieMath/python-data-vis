3a468f9064f3e98e4beafef0d91cd99f1b21a619
Yuxin Ye <yuxin_ye@protonmail.com>
2022-06-29 11:53:44 +1000

    add 3d plot

--- main.py
+++ main.py
@@ -8,11 +8,19 @@ from mpl_toolkits import mplot3d
 # plt.show()
 
 # Single Points Scattered
+# ax = plt.axes(projection="3d")
+# x_data = np.random.randint(0, 100, (500, ))
+# y_data = np.random.randint(0, 100, (500, ))
+# z_data = np.random.randint(0, 100, (500, ))
+# ax.scatter(x_data, y_data, z_data, alpha=0.1)
+# plt.show()
+
+# Single Points Function
 ax = plt.axes(projection="3d")
-x_data = np.random.randint(0, 100, (500, ))
-y_data = np.random.randint(0, 100, (500, ))
-z_data = np.random.randint(0, 100, (500, ))
-ax.scatter(x_data, y_data, z_data, alpha=0.1)
+x_data = np.arange(0, 50, 0.1)
+y_data = np.arange(0, 50, 0.1)
+z_data = x_data * y_data
+ax.plot(x_data, y_data, z_data)
 plt.show()
 
 # Scatter Plots
