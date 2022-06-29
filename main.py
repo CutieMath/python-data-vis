b9f47416a2c52b2e857dc2c50c68a10b55111eba
Yuxin Ye <yuxin_ye@protonmail.com>
2022-06-29 11:32:02 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits import mplot3d
+
+# Single Points basic
+ax = plt.axes(projection="3d")
+ax.scatter(3, 5, 7)
+plt.show()
+
+# Scatter Plots
