4910b58561606e1444ac4c10cb70a116d488d8c7
Yuxin Ye <yuxin_ye@protonmail.com>
2022-06-29 11:55:40 +1000

    plot sin and cos data

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from mpl_toolkits import mplot3d
 ax = plt.axes(projection="3d")
 x_data = np.arange(0, 50, 0.1)
 y_data = np.arange(0, 50, 0.1)
-z_data = x_data * y_data
+z_data = np.sin(x_data) * np.cos(y_data)
 ax.plot(x_data, y_data, z_data)
 plt.show()
 
